46d90e128894f6b86d3aa4a041b01946ab440f5f
Víctor Blázquez <99849087+victorblaz1998@users.noreply.github.com>
2022-04-06 14:56:31 +0200

    Update Casilla.cpp

--- Ajedrez/src/Casilla.cpp
+++ Ajedrez/src/Casilla.cpp
@@ -1,1 +1,34 @@
 #include "Casilla.h"
+#include <math.h>
+
+Casilla::Casilla(int x, int y){
+  f = x;
+  c = y;
+}
+
+Casilla::~Casilla(){
+}
+
+Casilla Casilla::operator +(Casilla cas){
+}
+
+Casilla Casilla::operator +(float num){
+}
+
+Casilla Casilla::operator -(Casilla cas){
+}
+
+Casilla Casilla::operator *(float num){
+}
+
+float Casilla::operator *(Casilla cas){
+}
+
+Casilla Casilla:unitario(){
+}
+
+float Casilla::argumento(){
+}
+
+float Casilla::modulo(){
+}
