616e75531a3e2fa0a542be85d742e02d6e3a4029
PBernia <63584074+PBernia@users.noreply.github.com>
2022-04-15 19:36:07 +0200

    Movimiento del caballo

--- Ajedrez/src/Caballo.cpp
+++ Ajedrez/src/Caballo.cpp
@@ -1,1 +1,11 @@
 #include "Caballo.h"
+#include <math.h>
+bool Caballo::Mov(Casilla cd, Casilla co)
+{
+	if (((abs(cd.f - co.f) == 2) && (abs(cd.c - co.c) == 1)) || ((abs(cd.f - co.f) == 2) && (abs(cd.c - co.c) == 1))) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
