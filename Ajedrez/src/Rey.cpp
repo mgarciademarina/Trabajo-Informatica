caf8b467de994a45f2078808b0ca89706a52cce9
PBernia <63584074+PBernia@users.noreply.github.com>
2022-04-15 19:19:57 +0200

    Movimientos básicos del rey

--- Ajedrez/src/Rey.cpp
+++ Ajedrez/src/Rey.cpp
@@ -1,1 +1,7 @@
+
 #include "Rey.h"
+#include <math.h>
+bool Rey::Mov(Casilla cd, Casilla co) {
+	if ((abs(cd.f - co.f) <= 1) && (abs(cd.c - co.c)<=1) && ((cd.c != co.c) || (cd.f != co.f))){ return true;}
+	else { return false; }
+}
