076da0946152f356bfa553c121c1736c1ef0f70c
PBernia <63584074+PBernia@users.noreply.github.com>
2022-04-16 14:05:42 +0200

    Corrección de errores en el movimiento

--- Ajedrez/src/Peon.cpp
+++ Ajedrez/src/Peon.cpp
@@ -2,14 +2,14 @@
 #include <math.h>
 bool Peon::Mov(Casilla cd, Casilla co)
 {
-	if ((cd.c=co.c)&&((cd.f == (co.f + 1)) || ((cd.f == (co.f + 2)) && (co.f == 2)))) { return true; } //Para blanco (pieza.color == BLANCO) &&
-	else if ((cd.c = co.c) && ((cd.f == (co.f - 1)) || ((cd.f == (co.f - 2))) && (co.f == 6))) { return true; } //Para negro (pieza.color == NEGRO) &&
+	if ((cd.c=co.c)&&(((cd.f-co.f)==1)||((cd.f =4 )))){ return true; } //Para blanco (pieza.color == BLANCO) &&
+	else if ((cd.c = co.c) && (((co.f - cd.f) == 1) || ((cd.f = 5)))) { return true; } //Para negro (pieza.color == NEGRO) &&
 	else { return false; }
 }
 bool Peon::Comer(Casilla cd, Casilla co)
 {
 	return true; // Necesito saber si hay una pieza de otro color 
-	/* if ((pieza.color == BLANCO) && (abs(cd.c - co.c) == 1) && ((cd.f == (co.f + 1)) || ((cd.f == (co.f + 2) && (co.f == 2))))) { return true; }
-	else if ((pieza.color == NEGRO)&&(abs(cd.c-co.c)==1)&&((cd.f == (co.f - 1)) || ((cd.f == (co.f - 2) && (co.f == 6))))){return true;}
+	/* if ((pieza.color == BLANCO) && (abs(cd.c - co.c) == 1) && (((cd.f-co.f)==1)||((cd.f =4 )))) { return true; }
+	else if ((pieza.color == NEGRO)&&(abs(cd.c-co.c)==1)&&(((co.f - cd.f) == 1) || ((cd.f = 5)))){return true;}
 	else { return false; }*/
 }
