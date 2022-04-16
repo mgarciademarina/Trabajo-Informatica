62b53102bf399036b5a814db0cdd55cbc66e4b1c
PBernia <63584074+PBernia@users.noreply.github.com>
2022-04-16 13:54:30 +0200

    Corrección de error en el movimiento

--- Ajedrez/src/Caballo.cpp
+++ Ajedrez/src/Caballo.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 bool Caballo::Mov(Casilla cd, Casilla co)
 {
-	if (((abs(cd.f - co.f) == 2) && (abs(cd.c - co.c) == 1)) || ((abs(cd.f - co.f) == 2) && (abs(cd.c - co.c) == 1))) {
+	if (((abs(cd.f - co.f) == 2) && (abs(cd.c - co.c) == 1)) || ((abs(cd.f - co.f) == 1) && (abs(cd.c - co.c) == 2))) {
 		return true;
 	}
 	else {
