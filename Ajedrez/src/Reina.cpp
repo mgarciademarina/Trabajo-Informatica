95c20d293cdb0ebbb13b381f4908394237dea377
PBernia <63584074+PBernia@users.noreply.github.com>
2022-04-16 13:50:38 +0200

    Diagonales las hace bien

--- Ajedrez/src/Reina.cpp
+++ Ajedrez/src/Reina.cpp
@@ -1,6 +1,7 @@
 #include "Reina.h"
+#include <math.h>
 bool Reina::Mov(Casilla cd, Casilla co)
 {
-	if ((cd.f == co.f || cd.c == co.c)||((cd.f - co.f) == (cd.c - co.c))){ return true; }
+	if ((cd.f == co.f || cd.c == co.c)||(abs(cd.f - co.f) == abs(cd.c - co.c))){ return true; }
 	else {return false;}
 }
