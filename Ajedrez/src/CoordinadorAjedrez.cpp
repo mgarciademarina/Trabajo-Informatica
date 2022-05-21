2be0b58a34695dd8692d3a343863c5de22e95cf7
Rafael Rodríguez <rafael.rodriguezp@alumnos.upm.es>
2022-05-21 16:21:40 +0200

    otros

--- Ajedrez/src/CoordinadorAjedrez.cpp
+++ Ajedrez/src/CoordinadorAjedrez.cpp
@@ -21,7 +21,7 @@ void CoordinadorAjedrez::dibuja() {
 		ETSIDI::setFont("fuentes/fuente2.otf", 12);
 		ETSIDI::printxy("PULSE LA TECLA -E- PARA EMPEZAR", -5, 7);
 		ETSIDI::printxy("PULSE LA TECLA -S- PARA SALIR", -5, 6);
-		ETSIDI::printxy("PULSE LA TECLA -L- PARA CARGAR LA ÚLTIMA PARTIDA GUARDADA", -5, 5);
+		ETSIDI::printxy("PULSE LA TECLA -L- PARA CARGAR LA ULTIMA PARTIDA GUARDADA", -5, 5);
 		ETSIDI::printxy("WenasnoCHESS", 2, 1);
 	}
 	else if (estado == JUEGO) {
